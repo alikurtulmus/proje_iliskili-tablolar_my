a6a2831d28ab9d7793086ff2abaec56c3a0661b3
Ali Kurtulmus <mkurtulmus.eng@gmail.com>
2022-08-29 22:25:25 +0300

    Add project files.

---
+++ proje_iliskili-tablolar_my/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace proje_iliskili_tablolar_my
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti=new SqlConnection("Data Source=DESKTOP-QBFPK40\\SQLEXPRESS;Initial Catalog=yb_hareket;Integrated Security=True");
+        //Data Source=DESKTOP-QBFPK40\SQLEXPRESS;Initial Catalog=yb_hareket;Integrated Security=True
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("Execute Proje6",baglanti);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+    }
+}
